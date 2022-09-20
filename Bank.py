295cbd2c44a448a385a5d6c03d1fb4591e95bd6a
Zulfiqarrafi <113327076+Zulfiqarrafi@users.noreply.github.com>
2022-09-20 10:16:56 +0700

    Add files via upload

---
+++ Bank.py
@@ -0,0 +1,10 @@
+opener = input("how the receptionist greets ?").strip().lower()
+if opener.startswith("hello") or opener == "hello": 
+    print("0$")
+elif opener.startswith("hello julian"): 
+    print("20$")
+else :
+    print("100$")
+
+#Nama: Muhammad Zulfiqar Rafi
+#Nim:220535610308
